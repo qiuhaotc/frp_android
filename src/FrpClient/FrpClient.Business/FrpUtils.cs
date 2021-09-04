74c21e16fe8b61a330c859c8c708c641552a5414
qiuhaotc <qiu66hao@163.com>
2021-09-04 11:29:02 +0800

    initialize

---
+++ src/FrpClient/FrpClient.Business/FrpUtils.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace FrpClient.Business
+{
+    public static class FrpUtils
+    {
+        public static Process StartFrp(bool isFrps, string configuration, string location)
+        {
+            var cpuArchitecture = GetArchitechture(Android.OS.Build.SupportedAbis);
+            var embeddedSource = typeof(FrpUtils).Namespace + (isFrps ? $".Assests.{cpuArchitecture}.frpc" : $".Assests.{cpuArchitecture}.frps");
+            var frpConfigFilePath = Path.Combine(location, isFrps ? "frps.ini" : "frpc.ini");
+            var frpExecutableFilePath = Path.Combine(location, isFrps ? "frps" : "frpc");
+            File.WriteAllText(frpConfigFilePath, configuration);
+
+            var assembly = typeof(FrpUtils).Assembly;
+            var resFilestream = assembly.GetManifestResourceStream(embeddedSource);
+
+            if (File.Exists(frpExecutableFilePath))
+            {
+                File.Delete(frpExecutableFilePath);
+            }
+
+            using (var file = new FileStream(frpExecutableFilePath, FileMode.Create, FileAccess.Write))
+            {
+                resFilestream.CopyTo(file);
+            }
+
+            var cmd = new[] { "chmod", "744", frpExecutableFilePath };
+            Java.Lang.Runtime.GetRuntime().Exec(cmd);
+
+            var process = Process.Start(new ProcessStartInfo
+            {
+                FileName = frpExecutableFilePath,
+                Arguments = $"-c {frpConfigFilePath}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                CreateNoWindow = false,
+            });
+
+            return process;
+        }
+
+        public static Java.Lang.Process StartFrp2(bool isFrps, string configuration, string location)
+        {
+            var cpuArchitecture = GetArchitechture(Android.OS.Build.SupportedAbis);
+            var embeddedSource = typeof(FrpUtils).Namespace + (isFrps ? $".Assests.{cpuArchitecture}.frpc" : $".Assests.{cpuArchitecture}.frps");
+            var frpConfigFilePath = Path.Combine(location, isFrps ? "frps.ini" : "frpc.ini");
+            var frpExecutableFilePath = Path.Combine(location, isFrps ? "frps" : "frpc");
+            File.WriteAllText(frpConfigFilePath, configuration);
+
+            var assembly = typeof(FrpUtils).Assembly;
+
+            if (File.Exists(frpExecutableFilePath))
+            {
+                File.Delete(frpExecutableFilePath);
+            }
+
+            var resFilestream = assembly.GetManifestResourceStream(embeddedSource);
+            using (var file = new FileStream(frpExecutableFilePath, FileMode.Create, FileAccess.Write))
+            {
+                resFilestream.CopyTo(file);
+            }
+
+            var process2 = Java.Lang.Runtime.GetRuntime().Exec(new[] { "chmod", "777", frpExecutableFilePath });
+            var process3 = Java.Lang.Runtime.GetRuntime().Exec(new[] { "chmod", "777", frpConfigFilePath });
+            var process = Java.Lang.Runtime.GetRuntime().Exec(new[] { frpExecutableFilePath, "-c", frpConfigFilePath });
+            return process;
+        }
+
+        static string GetArchitechture(IList<string> supportedAbis)
+        {
+            var arch = "armFrp";
+
+            if (supportedAbis != null)
+            {
+                if (supportedAbis.Contains("x86") || supportedAbis.Contains("amd64"))
+                {
+                    arch = "amd64";
+                }
+                else if (supportedAbis.Contains("arm64"))
+                {
+                    arch = "arm64Frp";
+                }
+            }
+
+            return arch;
+        }
+    }
+}
---
+++ src/FrpClient/FrpClient.UI/MainActivity.cs
@@ -0,0 +1,100 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using FrpClient.Business;
+using Google.Android.Material.BottomNavigation;
+
+namespace FrpClient.UI
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        Button startButton;
+        Button stopButton;
+        EditText config;
+        EditText logs;
+        System.Diagnostics.Process process;
+        Java.Lang.Process process2;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+
+            startButton = FindViewById<Button>(Resource.Id.buttonStart);
+            startButton.Click += StartButton_Click;
+            stopButton = FindViewById<Button>(Resource.Id.buttonStop);
+            stopButton.Click += StartButton_Click1;
+            config = FindViewById<EditText>(Resource.Id.editTextForConfig);
+            logs = FindViewById<EditText>(Resource.Id.logs);
+        }
+
+        void StartButton_Click1(object sender, System.EventArgs e)
+        {
+            try
+            {
+                process?.Kill();
+                process2?.Destroy();
+            }
+            catch (System.Exception ex)
+            {
+                logs.Text = logs.Text + System.Environment.NewLine + ex;
+            }
+
+            process = null;
+        }
+
+        void StartButton_Click(object sender, System.EventArgs e)
+        {
+            StartButton_Click1(null, null);
+
+            var filePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var isFrps = FindViewById<RadioButton>(Resource.Id.frpsType).Checked;
+
+            process2 = FrpUtils.StartFrp2(isFrps, config.Text, filePath);
+            var output = process2.IsAlive;
+
+            process = FrpUtils.StartFrp(isFrps, config.Text, filePath);
+            var b = process.ExitCode;
+
+            //while (!process.StandardOutput.EndOfStream)
+            //{
+            //    string line = process.StandardOutput.ReadLine();
+            //    // do something with line
+            //    logs.Text = logs.Text + System.Environment.NewLine + line;
+            //}
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    return true;
+
+                case Resource.Id.navigation_dashboard:
+                    return true;
+
+                case Resource.Id.navigation_notifications:
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
+
